9d04916ffc110b3c56a42c7fd14df001c87560c8
RaffayIrfan <78643689+RaffayIrfan@users.noreply.github.com>
2025-09-29 19:09:52 +0500

    changes

---
+++ Readme.md
@@ -0,0 +1,15 @@
+#cs117-lab3
+
+##What did you notice about registers and instructions?
+Regliters store the value of memory spaces where the value is to be stored.Instructions are steps performed on the regiters calling them with an value given to the arithmetic logic unit (alu).
+
+##How is coding in Assembly different from Python?
+Assembly is a low level language while python is a high level language.Assembly performs direct operation through the cpu while python requires more steps for it to be understandable to the computer. Assembly is more like plotting bits and python is like formal language.
+
+#Why is Python easier/faster for building the same project?
+Python is in formal language thats why it is easier for uilding the same project.
+
+#Which features of Python help abstraction (variables, functions, loops)?
+Python is feature rich and provides absraction.Most features of python provide abstraction
+
+repo link:https://github.com/Pikachu889/Applications-of-ICT-Lab-group-16
--- python_calc.py
+++ python_calc.py
@@ -1,2 +1,59 @@
 
 a= int(input("Enter First Number:"))
+b= int(input("Enter Second Number:"))
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b  
+
+def mul(a,b):
+    return a*b  
+
+def div(a,b):
+    return a/b
+
+def is_even(a):
+    if a%2==0:
+        return True
+    else:
+        return False
+
+def percent(a,b):
+    return (a/b)*100
+
+loop=True
+
+while loop:
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.Is Even")
+    print("6.Percentage")
+    print("7.Exit")
+    choice=int(input("Enter your choice:"))
+    if choice==1:
+        print("Addition is:",add(a,b))
+    elif choice==2:
+        print("Subtraction is:",sub(a,b))
+    elif choice==3:
+        print("Multiplication is:",mul(a,b))
+    elif choice==4:
+        print("Division is:",div(a,b))
+    elif choice==5:
+        if is_even(a):
+            print(a,"is Even")
+        else:
+            print(a,"is Odd")
+        if is_even(b):
+            print(b,"is Even")
+        else:
+            print(b,"is Odd")
+    elif choice==6:
+        print("Percentage of",a,"over",b,"is:",percent(a,b),"%")
+    elif choice==7:
+        loop=False
+    else:
+        print("Invalid Input")
